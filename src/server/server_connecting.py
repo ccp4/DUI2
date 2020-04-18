b4387cd17654bfd315fda688986bb2d9d44840dc
luisodls <luis.fuentes-montero@diamond.ac.uk>
2020-04-18 11:57:56 +0100

    starting to connect multi node with server side code

--- src/server/server_connecting.py
+++ src/server/server_connecting.py
@@ -3,9 +3,13 @@ import socketserver
 from urllib.parse import urlparse, parse_qs
 import time, subprocess
 
+import out_utils, multi_node
+
+
 class ReqHandler(http.server.BaseHTTPRequestHandler):
 
     def do_GET(self):
+        print("lst_test =", lst_test)
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
@@ -14,7 +18,19 @@ class ReqHandler(http.server.BaseHTTPRequestHandler):
         url_path = self.path
         dict_cmd = parse_qs(urlparse(url_path).query)
         cmd_str = dict_cmd['command'][0]
-        cmd_lst = cmd_str.split(' ')
+
+        lst_cmd = cmd_str.split(";")
+
+        lst_cmd_lst = []
+        for sub_str in lst_cmd:
+            lst_cmd_lst.append(sub_str.split(" "))
+
+        print("lst_cmd_lst:", lst_cmd_lst)
+        cmd_tree_runner.run(lst_cmd_lst)
+        tree_output(cmd_tree_runner)
+
+
+        '''
         try:
             print("\n Running:", cmd_lst, "\n")
             proc = subprocess.Popen(
@@ -35,11 +51,53 @@ class ReqHandler(http.server.BaseHTTPRequestHandler):
         except:
             print("error running subprocess from server")
 
+        '''
+
         print("sending /*EOF*/")
         self.wfile.write(bytes('/*EOF*/', 'utf-8'))
 
+        lst_test.append(cmd_str)
+
+
+tree_output = out_utils.TreeShow()
+cmd_tree_runner = multi_node.Runner()
+'''
+if __name__ == "__main__":
+
+    command = ""
+
+    while command.strip() != "exit" and command.strip() != "quit":
+        try:
+            inp_str = "lin [" + str(cmd_tree_runner.current_line) + "] >>> "
+            command = str(input(inp_str))
+
+        except EOFError:
+            print("Caught << EOFError >> \n  ... interrupting")
+            sys.exit(0)
+
+        except:
+            print("Caught << some error >>", e, " ... interrupting")
+            sys.exit(1)
+
+        print("command =", command)
+
+        lst_cmd = command.split(";")
+
+        lst_cmd_lst = []
+        for sub_str in lst_cmd:
+            lst_cmd_lst.append(sub_str.split(" "))
+
+        print("lst_cmd_lst:", lst_cmd_lst)
+        cmd_tree_runner.run(lst_cmd_lst)
+
+        #out_utils.print_list(cmd_tree_runner.step_list, cmd_tree_runner.current_line)
+
+        tree_output(cmd_tree_runner)
+'''
+
 
 if __name__ == "__main__":
+    lst_test = []
     PORT = 8080
     with socketserver.ThreadingTCPServer(("", PORT), ReqHandler) as http_daemon:
         print("serving at port", PORT)
