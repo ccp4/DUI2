298d447659d88f9d84f8ee4cc2b7905807e57fa3
luisodls <luis.fuentes-montero@diamond.ac.uk>
2020-05-18 12:35:58 +0100

    small aesthetic change, part 2

--- src/client/tree_draw_tmp.py
+++ src/client/tree_draw_tmp.py
@@ -1,12 +1,11 @@
-
-
 class TreeShow(object):
     def __init__(self):
-        self.ind_spc = "      "
-        self.ind_lin = "------"
+        self.ind_spc = "    "
+        self.ind_lin = "----"
 
     def __call__(self, new_lst_nod = None):
         self.lst_nod = new_lst_nod
+
 
         self.lst_out = []
         self.lst_out.append("")
@@ -14,9 +13,9 @@ class TreeShow(object):
         self.lst_out.append(" | ")
         self.lst_out.append(" |  line number ")
         self.lst_out.append(" |   | ")
-        self.lst_out.append(" |   |  command ")
-        self.lst_out.append(" |   |   | ")
+        self.lst_out.append(" |   |    command --->  ")
         self.lst_out.append("--------------------------")
+
         self.max_indent = 0
         self.str_lst = []
         self._add_tree(step = self.lst_nod[0], indent=0)
@@ -36,11 +35,13 @@ class TreeShow(object):
         else:
             stp_prn = " R "
 
-        str_lin_num = "{0:3}".format(int(step["lin_num"]))
-        stp_prn += str_lin_num + self.ind_spc * indent + r"   \___"
-        stp_prn += str(step["cmd2show"][0]) + "     "
+        str_lin_num = "(" + str(step["lin_num"]) + ")"
+        stp_prn += self.ind_spc * indent + r"  \__"
 
-        self.str_lst.append([stp_prn, indent, int(step["lin_num"])])
+        stp_prn += str_lin_num + "     "
+        str_cmd = str(step["cmd2show"][0])
+
+        self.str_lst.append([stp_prn, indent, int(step["lin_num"]), str_cmd])
         new_indent = indent
 
         if len(step["next_step_list"]) > 0:
@@ -56,24 +57,25 @@ class TreeShow(object):
                 self.max_indent = new_indent
 
     def _output_connect(self):
-        tree_str_dat = []
-        for tmp_lst in self.str_lst:
-            tree_str_dat.append(tmp_lst)
-
-        for pos, loc_lst in enumerate(tree_str_dat):
+        for pos, loc_lst in enumerate(self.str_lst):
             if pos > 0:
-                if loc_lst[1] < tree_str_dat[pos - 1][1]:
+                if loc_lst[1] < self.str_lst[pos - 1][1]:
                     for up_pos in range(pos - 1, 0, -1):
-                        pos_in_str = loc_lst[1] * len(self.ind_spc) + 9
-                        left_side = tree_str_dat[up_pos][0][0:pos_in_str]
-                        right_side = tree_str_dat[up_pos][0][pos_in_str + 1 :]
-                        if tree_str_dat[up_pos][1] > loc_lst[1]:
-                            tree_str_dat[up_pos][0] = left_side + "|" + right_side
+                        pos_in_str = loc_lst[1] * len(self.ind_spc) + 5
+                        left_side = self.str_lst[up_pos][0][0:pos_in_str]
+                        right_side = self.str_lst[up_pos][0][pos_in_str + 1 :]
+                        if self.str_lst[up_pos][1] > loc_lst[1]:
+                            self.str_lst[up_pos][0] = left_side + "|" + right_side
 
-                        elif tree_str_dat[up_pos][1] == loc_lst[1]:
+                        elif self.str_lst[up_pos][1] == loc_lst[1]:
                             break
 
-        for prn_str in tree_str_dat:
+            lng = len(self.ind_spc) * self.max_indent + 22
+            lng_lft = lng - len(self.str_lst[pos][0])
+            str_here = lng_lft * " "
+            self.str_lst[pos][0] += str_here + " | " + self.str_lst[pos][3]
+
+        for prn_str in self.str_lst:
             self.lst_out.append(prn_str[0])
 
         self.lst_out.append("---------------------" + self.max_indent * self.ind_lin)
@@ -81,4 +83,5 @@ class TreeShow(object):
     def print_output(self):
         for prn_str in self.lst_out:
             print(prn_str)
+
 
