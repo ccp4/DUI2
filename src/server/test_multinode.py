42324c84d331702de28e720fcf074d2bd1bc4bab
luisodls <luis.fuentes-montero@diamond.ac.uk>
2020-05-22 14:04:05 +0100

    add one more test

--- src/server/test_multinode.py
+++ src/server/test_multinode.py
@@ -1,26 +1,36 @@
 from multi_node import Runner
 
+lst_cmd1= [
+"0 ip x4",
+"0 ip x41",
+"2 ls",
+"1 ls",
+"1 2 ls",
+]
+
+lst_cmd2 = [
+"0 ip x4",
+"0 ip x41",
+"0 ip x42",
+"1 fd nproc=9",
+"2 fd nproc=9",
+"3 fd nproc=9",
+"4 id",
+"5 id",
+"6 id",
+"7 rf",
+"8 rf",
+"9 rf",
+"10 11 12 ce",
+"13 it nproc=4"
+]
+
 if __name__ == "__main__":
-    lst_cmd = [
-    "0 ip x4",
-    "0 ip x41",
-    "0 ip x42",
-    "1 fd nproc=9",
-    "2 fd nproc=9",
-    "3 fd nproc=9",
-    "4 id",
-    "5 id",
-    "6 id",
-    "7 rf",
-    "8 rf",
-    "9 rf",
-    "10 11 12 ce",
-    "13 it nproc=4"
-    ]
 
     cmd_tree_runner = Runner(None)
     cmd_tree_runner.run("display")
 
-    for cmd_str in lst_cmd:
+    # swap lst_cmd1 with lst_cmd2 to select test
+    for cmd_str in lst_cmd1:
         cmd_tree_runner.run(cmd_str)
         cmd_tree_runner.run("display")
