f6b009dd28a35ad6e5044b70c0672606130bbd65
luisodls <luis.fuentes-montero@diamond.ac.uk>
2020-05-29 13:34:58 +0100

    enhancing some tests

--- src/client/client_connecting_cli.py
+++ src/client/client_connecting_cli.py
@@ -3,20 +3,32 @@ import requests
 
 lst_cmd = [
             {"lin2go_lst":["0"], "cmd_lst":["ip x4"]},
-            {"lin2go_lst":["1"], "cmd_lst":["fd nproc=9"]},
-            {"lin2go_lst":["2"], "cmd_lst":["id"]},
-            {"lin2go_lst":["3"], "cmd_lst":["rf"]},
-            {"lin2go_lst":["4"], "cmd_lst":["it"]},
-            {"lin2go_lst":[""], "cmd_lst":["d"]},
             {"lin2go_lst":["0"], "cmd_lst":["ip x41"]},
-            {"lin2go_lst":["6"], "cmd_lst":["fd nproc=9"]},
-            {"lin2go_lst":["7"], "cmd_lst":["id"]},
-            {"lin2go_lst":["8"], "cmd_lst":["rf"]},
-            {"lin2go_lst":["9"], "cmd_lst":["it"]},
-            {"lin2go_lst":["5", "10"], "cmd_lst":["ce"]},
-            {"lin2go_lst":["11"], "cmd_lst":["sm"]},
+            {"lin2go_lst":["0"], "cmd_lst":["ip x42"]},
+            {'lin2go_lst': [1], 'cmd_lst':
+                 [['dials.generate_mask untrusted.rectangle=0,1421,1258,1312 output.mask=tmp_mask.pickle'],
+                  ['dials.apply_mask input.mask=tmp_mask.pickle']]},
+            {'lin2go_lst': [2], 'cmd_lst':
+                 [['dials.generate_mask untrusted.rectangle=0,1421,1258,1312 output.mask=tmp_mask.pickle'],
+                  ['dials.apply_mask input.mask=tmp_mask.pickle']]},
+            {'lin2go_lst': [3], 'cmd_lst':
+                 [['dials.generate_mask untrusted.rectangle=0,1421,1258,1312 output.mask=tmp_mask.pickle'],
+                  ['dials.apply_mask input.mask=tmp_mask.pickle']]},
             {"lin2go_lst":[""], "cmd_lst":["d"]},
+            {"lin2go_lst":["4"], "cmd_lst":["fd nproc=9"]},
+            {"lin2go_lst":["5"], "cmd_lst":["fd nproc=9"]},
+            {"lin2go_lst":["6"], "cmd_lst":["fd nproc=9"]},
+            {"lin2go_lst":[""], "cmd_lst":["d"]},
+            {"lin2go_lst":["7"], "cmd_lst":["id"]},
+            {"lin2go_lst":["8"], "cmd_lst":["id"]},
+            {"lin2go_lst":["9"], "cmd_lst":["id"]},
+            {"lin2go_lst":[""], "cmd_lst":["d"]},
+            {"lin2go_lst":["10","11","12"], "cmd_lst":["ce"]},
+            {"lin2go_lst":["13"], "cmd_lst":["rf"]},
+            {"lin2go_lst":[""], "cmd_lst":["d"]},
+            {"lin2go_lst":["14"], "cmd_lst":["it nproc=3"]},
           ]
+
 
 if __name__ == "__main__":
     for cmd in lst_cmd:
