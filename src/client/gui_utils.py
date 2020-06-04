fa45377900fab1046c3c650259d6e1662e5adf9b
luisodls <luis.fuentes-montero@diamond.ac.uk>
2020-06-04 10:19:01 +0100

    swapping the position of old text output example

--- src/client/gui_utils.py
+++ src/client/gui_utils.py
@@ -6,37 +6,6 @@ from PySide2 import QtUiTools
 from PySide2.QtGui import *
 
 
-old_graph = '''
-status: (R)eady  (B)usy  (F)ailed  (S)ucceeded
- |
- |  line number
- |   |
- |   |    command --->
---------------------------
- S   \_(0)                                   | Root
- S       \_(1)                               | ls
- S       |   \_(4)                           | ls
- S       |       \_(7) ``\```\```\           | ls
- S       |       \_(10)``\```\```\           | ls
- S       |       \_(11)  :   :   :           | ls
- S       |       \_(12)  :   :   :           | ls
- S       \_(2)           :   :   :           | ls
- S       |   \_(5)       :   :   :           | ls
- S       |       \_(8) ``\```\```\           | ls
- S       |       \_(13)``\```\```\           | ls
- S       |       \_(14)  :   :   :           | ls
- S       \_(3)           :   :   :           | ls
- S       |   \_(6)       :   :   :           | ls
- S       |       \_(9)   :   :   :           | ls
- S       |       \_(15)  :   :   :           | ls
- S       |           \_(16)  :   :           | ls,  parents:[7, 10, 8, 13, 15]
- S       |           \_____(17)  :           | ls,  parents:[7, 10, 8, 13, 15]
- S       |           \_________(18)          | ls,  parents:[15, 13, 8, 10, 7]
- F       \_(19)                              | 0
- R       \_(20)                              | None
-----------------------------------------------------
-'''
-
 nod_lst = [
 {"lin_num": 0, "status": "Succeeded", "cmd2show": ["Root"], "child_node_lst": [1, 2, 3, 19, 20], "parent_node_lst": []},
 {"lin_num": 1, "status": "Succeeded", "cmd2show": ["ls"], "child_node_lst": [4], "parent_node_lst": [0]},
@@ -145,3 +114,35 @@ if __name__ == "__main__":
     m_obj = MainObject()
     m_obj.draw_graph(nod_lst)
     sys.exit(app.exec_())
+
+
+old_graph = '''
+status: (R)eady  (B)usy  (F)ailed  (S)ucceeded
+ |
+ |  line number
+ |   |
+ |   |    command --->
+--------------------------
+ S   \_(0)                                   | Root
+ S       \_(1)                               | ls
+ S       |   \_(4)                           | ls
+ S       |       \_(7) ``\```\```\           | ls
+ S       |       \_(10)``\```\```\           | ls
+ S       |       \_(11)  :   :   :           | ls
+ S       |       \_(12)  :   :   :           | ls
+ S       \_(2)           :   :   :           | ls
+ S       |   \_(5)       :   :   :           | ls
+ S       |       \_(8) ``\```\```\           | ls
+ S       |       \_(13)``\```\```\           | ls
+ S       |       \_(14)  :   :   :           | ls
+ S       \_(3)           :   :   :           | ls
+ S       |   \_(6)       :   :   :           | ls
+ S       |       \_(9)   :   :   :           | ls
+ S       |       \_(15)  :   :   :           | ls
+ S       |           \_(16)  :   :           | ls,  parents:[7, 10, 8, 13, 15]
+ S       |           \_____(17)  :           | ls,  parents:[7, 10, 8, 13, 15]
+ S       |           \_________(18)          | ls,  parents:[15, 13, 8, 10, 7]
+ F       \_(19)                              | 0
+ R       \_(20)                              | None
+----------------------------------------------------
+'''
