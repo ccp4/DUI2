b69927e2cce6ee53f069adcb284e49408fbe2154
luisodls <luis.fuentes-montero@diamond.ac.uk>
2020-06-07 16:44:01 +0100

    creaing a first rustic main client app

---
+++ src/client/main_client.py
@@ -0,0 +1,70 @@
+nod_lst = [{"lin_num": 0, "status": "Succeeded", "cmd2show": ["Root"], "child_node_lst": [], "parent_node_lst": []}]
+
+new_nod_lst = [
+{"lin_num": 0, "status": "Succeeded", "cmd2show": ["Root"], "child_node_lst": [1, 2, 3, 19, 20], "parent_node_lst": []},
+{"lin_num": 1, "status": "Succeeded", "cmd2show": ["ls"], "child_node_lst": [4], "parent_node_lst": [0]},
+{"lin_num": 2, "status": "Succeeded", "cmd2show": ["ls"], "child_node_lst": [5], "parent_node_lst": [0]},
+{"lin_num": 3, "status": "Succeeded", "cmd2show": ["ls"], "child_node_lst": [6], "parent_node_lst": [0]},
+{"lin_num": 4, "status": "Succeeded", "cmd2show": ["ls"], "child_node_lst": [7, 10, 11, 12], "parent_node_lst": [1]},
+{"lin_num": 5, "status": "Succeeded", "cmd2show": ["ls"], "child_node_lst": [8, 13, 14], "parent_node_lst": [2]},
+{"lin_num": 6, "status": "Succeeded", "cmd2show": ["ls"], "child_node_lst": [9, 15], "parent_node_lst": [3]},
+{"lin_num": 7, "status": "Succeeded", "cmd2show": ["ls"], "child_node_lst": [16, 17, 18], "parent_node_lst": [4]},
+{"lin_num": 8, "status": "Succeeded", "cmd2show": ["ls"], "child_node_lst": [16, 17, 18], "parent_node_lst": [5]},
+{"lin_num": 9, "status": "Succeeded", "cmd2show": ["ls"], "child_node_lst": [], "parent_node_lst": [6]},
+{"lin_num": 10, "status": "Succeeded", "cmd2show": ["ls"], "child_node_lst": [16, 17, 18], "parent_node_lst": [4]},
+{"lin_num": 11, "status": "Succeeded", "cmd2show": ["ls"], "child_node_lst": [], "parent_node_lst": [4]},
+{"lin_num": 12, "status": "Succeeded", "cmd2show": ["ls"], "child_node_lst": [], "parent_node_lst": [4]},
+{"lin_num": 13, "status": "Succeeded", "cmd2show": ["ls"], "child_node_lst": [16, 17, 18], "parent_node_lst": [5]},
+{"lin_num": 14, "status": "Succeeded", "cmd2show": ["ls"], "child_node_lst": [], "parent_node_lst": [5]},
+{"lin_num": 15, "status": "Succeeded", "cmd2show": ["ls"], "child_node_lst": [16, 17, 18], "parent_node_lst": [6]},
+{"lin_num": 16, "status": "Succeeded", "cmd2show": ["ls"], "child_node_lst": [], "parent_node_lst": [7, 10, 8, 13, 15]},
+{"lin_num": 17, "status": "Succeeded", "cmd2show": ["ls"], "child_node_lst": [], "parent_node_lst": [7, 10, 8, 13, 15]},
+{"lin_num": 18, "status": "Succeeded", "cmd2show": ["ls"], "child_node_lst": [], "parent_node_lst": [15, 13, 8, 10, 7]},
+{"lin_num": 19, "status": "Failed", "cmd2show": ["0", "ls"], "child_node_lst": [], "parent_node_lst": [0]},
+{"lin_num": 20, "status": "Ready", "cmd2show": ["None"], "child_node_lst": [], "parent_node_lst": [0]}
+]
+
+import sys
+from PySide2.QtCore import *
+from PySide2.QtWidgets import *
+from PySide2 import QtUiTools
+from PySide2.QtGui import *
+
+from gui_utils import draw_inner_graph
+
+class MainObject(QObject):
+    def __init__(self, parent = None):
+        super(MainObject, self).__init__(parent)
+
+        self.window = QtUiTools.QUiLoader().load("tree_test.ui")
+
+        self.window.ButtonSelect.clicked.connect(self.on_select)
+        self.window.ButtonClear.clicked.connect(self.on_clear)
+        self.window.ButtonMkChild.clicked.connect(self.on_make)
+        self.window.show()
+
+        self.my_scene = QGraphicsScene()
+        self.window.graphicsView.setScene(self.my_scene)
+        self.draw_graph(nod_lst)
+
+    def draw_graph(self, new_nod_lst):
+        self.nod_lst = new_nod_lst
+        draw_inner_graph(self.my_scene, self.nod_lst)
+        #self.my_scene.update()
+
+    def on_select(self):
+        print("on_select")
+
+    def on_clear(self):
+        print("on_clear")
+
+    def on_make(self):
+        print("on_make")
+        self.draw_graph(new_nod_lst)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    m_obj = MainObject()
+    sys.exit(app.exec_())
+
