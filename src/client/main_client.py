79d83ec3e53d3e65507ea14730c21b3e251a9e90
luisodls <luis.fuentes-montero@diamond.ac.uk>
2020-06-07 21:00:05 +0100

    connecting new ui layout with runnable code

--- src/client/main_client.py
+++ src/client/main_client.py
@@ -32,6 +32,13 @@ from PySide2.QtGui import *
 
 from gui_utils import draw_inner_graph
 
+#from PySide2 import QtCore, QtWidgets, QtGui, QtNetwork
+#import sys
+import time, json
+import requests
+import tree_draw_tmp
+
+'''
 class MainObject(QObject):
     def __init__(self, parent = None):
         super(MainObject, self).__init__(parent)
@@ -67,4 +74,134 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     m_obj = MainObject()
     sys.exit(app.exec_())
+
+'''
+##################################################################################################
+
+
+class Run_n_Output(QThread):
+    line_out = Signal(str)
+    def __init__(self, request):
+        super(Run_n_Output, self).__init__()
+        self.request = request
+
+    def run(self):
+        line_str = ''
+        while True:
+            tmp_dat = self.request.raw.read(1)
+            single_char = str(tmp_dat.decode('utf-8'))
+            line_str += single_char
+            if single_char == '\n':
+                #print(line_str[:-1])
+                self.line_out.emit(line_str)
+                line_str = ''
+
+            elif line_str[-7:] == '/*EOF*/':
+                print('>>  /*EOF*/  <<')
+                self.line_out.emit(' \n /*EOF*/ \n')
+                break
+
+'''
+class Client(QtWidgets.QDialog):
+    def __init__(self, parent=None):
+        super(Client, self).__init__(parent)
+'''
+
+class MainObject(QObject):
+    def __init__(self, parent = None):
+        super(MainObject, self).__init__(parent)
+        self.window = QtUiTools.QUiLoader().load("tree_test.ui")
+        self.window.CmdSend2server.clicked.connect(self.request_launch)
+        self.tree_obj = tree_draw_tmp.TreeShow()
+
+        self.window.ButtonSelect.clicked.connect(self.on_select)
+        self.window.ButtonClear.clicked.connect(self.on_clear)
+        self.window.ButtonMkChild.clicked.connect(self.on_make)
+
+        self.window.show()
+        self.my_scene = QGraphicsScene()
+        self.window.graphicsView.setScene(self.my_scene)
+        self.draw_graph(nod_lst)
+
+    def draw_graph(self, new_nod_lst):
+        self.nod_lst = new_nod_lst
+        draw_inner_graph(self.my_scene, self.nod_lst)
+        #self.my_scene.update()
+
+    def on_select(self):
+        print("on_select")
+
+    def on_clear(self):
+        print("on_clear")
+
+    def on_make(self):
+        print("on_make")
+        self.draw_graph(new_nod_lst)
+
+    def on_make(self):
+        print("on_make")
+        self.draw_graph(new_nod_lst)
+
+
+    def add_line(self, new_line):
+        self.window.incoming_text.moveCursor(QTextCursor.End)
+        self.window.incoming_text.insertPlainText(new_line)
+        self.window.incoming_text.moveCursor(QTextCursor.End)
+
+    def run_ended(self):
+        print("run ended")
+
+        cmd = {"nod_lst":"", "cmd_lst":["display"]}
+        req_get = requests.get('http://localhost:8080/', stream = True, params = cmd)
+
+        str_lst = []
+
+        line_str = ''
+        while True:
+            tmp_dat = req_get.raw.read(1)
+            single_char = str(tmp_dat.decode('utf-8'))
+            line_str += single_char
+            if single_char == '\n':
+                str_lst.append(line_str[:-1])
+                line_str = ''
+
+            elif line_str[-7:] == '/*EOF*/':
+                print('>>  /*EOF*/  <<')
+                break
+
+        lst_nodes = json.loads(str_lst[1])
+        lst_str = self.tree_obj(lst_nod = lst_nodes)
+
+        for tree_line in lst_str:
+            self.add_line(tree_line + "\n")
+
+        print("show tree ended")
+
+    def request_launch(self):
+        cmd_str = str(self.window.CmdEdit.text())
+        print("cmd_str", cmd_str)
+        nod_str = str(self.window.NumLin.value())
+
+        cmd = {"nod_lst":nod_str, "cmd_lst":[cmd_str]}
+        req_get = requests.get('http://localhost:8080/', stream = True, params = cmd)
+
+        self.thrd = Run_n_Output(req_get)
+        self.thrd.line_out.connect(self.add_line)
+        self.thrd.finished.connect(self.run_ended)
+        self.thrd.start()
+
+'''
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    client = Client()
+    client.show()
+    sys.exit(client.exec_())
+'''
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    m_obj = MainObject()
+    sys.exit(app.exec_())
+
 
