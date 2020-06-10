25c4551485a8060faa2f0d7f9048ecdda36e9571
luisodls <luis.fuentes-montero@diamond.ac.uk>
2020-06-10 15:39:41 +0100

    adding pen to bezier curve

--- src/client/gui_utils.py
+++ src/client/gui_utils.py
@@ -26,7 +26,7 @@ def add_indent(nod_lst):
 
     return nod_lst
 
-def draw_bezier(scene_in, p1x, p1y, p4x, p4y, vertical = False):
+def draw_bezier(scene_in, p1x, p1y, p4x, p4y, vertical = False, lin_pen = Qt.blue):
     if vertical:
         p2x = p1x
         p2y = (p1y + p4y) / 2.0
@@ -56,31 +56,25 @@ def draw_bezier(scene_in, p1x, p1y, p4x, p4y, vertical = False):
         y2 = p2y + dy23 * float(pos)
         x3 = p3x + dx34 * float(pos)
         y3 = p3y + dy34 * float(pos)
-
-
         dx1 = (x2 - x1) / n_points
         dy1 = (y2 - y1) / n_points
-
         dx2 = (x3 - x2) / n_points
         dy2 = (y3 - y2) / n_points
-
         gx1 = x1 + dx1 * float(pos)
         gy1 = y1 + dy1 * float(pos)
-
         gx2 = x2 + dx2 * float(pos)
         gy2 = y2 + dy2 * float(pos)
-
         dx = (gx2 - gx1) / n_points
         dy = (gy2 - gy1) / n_points
-
         nx = gx1 + dx * float(pos)
         ny = gy1 + dy * float(pos)
 
         if pos > 0:
-            scene_in.addLine(x, y, nx, ny)
+            scene_in.addLine(x, y, nx, ny, lin_pen)
 
         x = nx
         y = ny
+
 
 def get_coords(row, col, ft_ht, ft_wd):
     return col * ft_wd * 2 + row * ft_wd / 4, row  * ft_ht * 2
@@ -92,6 +86,10 @@ def draw_inner_graph(scene_in, nod_lst):
     ft_ht = fm.height()
     red_pen = QPen(Qt.red)
     grayBrush = QBrush(Qt.gray, bs=Qt.SolidPattern)
+
+    #lin_pen = QPen(Qt.blue)
+    #blueBrush = QBrush(Qt.blue, bs=Qt.SolidPattern)
+    lin_pen = QPen(Qt.blue, 2, Qt.SolidLine)
 
     lst_w_indent = add_indent(nod_lst)
     for node in lst_w_indent:
@@ -114,7 +112,8 @@ def draw_inner_graph(scene_in, nod_lst):
                     scene_in,
                     my_parent_coord_x, my_parent_coord_y + ft_ht / 4,
                     my_coord_x, my_coord_y - ft_ht,
-                    vertical
+                    vertical,
+                    lin_pen
                 )
 
     for row, node in enumerate(lst_w_indent):
