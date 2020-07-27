11450657af2bd0cbf3283b8f64352b4487c1d6aa
luisodls <luis.fuentes-montero@diamond.ac.uk>
2020-07-27 18:20:32 +0100

    removing not needed stacked widget

--- src/client/main_client.py
+++ src/client/main_client.py
@@ -57,6 +57,50 @@ from simpler_param_widgets import (
 
 uni_url = 'http://localhost:8080/'
 
+widgets_defs = {
+    "import" : {
+        "main_cmd"  :"dials.import",
+        "nxt_widg_lst"  :["find_spots"]
+    },
+    "find_spots" : {
+        "main_cmd"  :"dials.find_spots",
+        "nxt_widg_lst"  :["index"]
+    },
+    "index" : {
+        "main_cmd"  :"dials.index",
+        "nxt_widg_lst"  :["refine_bravais_settings", "refine"]
+    },
+    "refine_bravais_settings" : {
+        "main_cmd"  :"dials.refine_bravais_settings",
+        "nxt_widg_lst"  :["reindex"]
+    },
+    "reindex" : {
+        "main_cmd"  :"dials.reindex",
+        "nxt_widg_lst"  :["refine", "integrate"]
+    },
+    "refine" : {
+        "main_cmd"  :"dials.refine",
+        "nxt_widg_lst"  :["integrate", "refine_bravais_settings"]
+    },
+    "integrate" : {
+        "main_cmd"  :"dials.integrate",
+        "nxt_widg_lst"  :["scale", "symmetry"]
+    },
+    "symmetry" : {
+        "main_cmd"  :"dials.symmetry",
+        "nxt_widg_lst"  :["scale"]
+    },
+    "scale" : {
+        "main_cmd"  :"dials.scale",
+        "nxt_widg_lst"  :["symmetry"]
+    },
+    "combine_experiments" : {
+        "main_cmd"  :"dials.combine_experiments",
+        "nxt_widg_lst"  :["scale", "symmetry"]
+    }
+}
+
+
 def json_data_request(url, cmd):
     try:
         req_get = requests.get(url, stream = True, params = cmd)
@@ -123,8 +167,6 @@ class MainObject(QObject):
         ui_path += os.sep + "client.ui"
         self.window = QtUiTools.QUiLoader().load(ui_path)
 
-        #############################################################################################
-        #############################################################################################
         imp_widg = ImportWidget()
         imp_widg.item_changed.connect(self.item_param_changed)
         self.window.ImportScrollArea.setWidget(imp_widg)
@@ -190,17 +232,7 @@ class MainObject(QObject):
         ce_advanced_parameters.item_changed.connect(self.item_param_changed)
         self.window.CombineAdvancedScrollArea.setWidget(ce_advanced_parameters)
 
-        #############################################################################################
-        self.param_widgets = {"import" : {"main_cmd"  :"dials.import"},
-                              "find_spots" : {"main_cmd"  :"dials.find_spots"},
-                              "index" : {"main_cmd"  :"dials.index"},
-                              "refine_bravais_settings" : {"main_cmd"  :"dials.refine_bravais_settings"},
-                              "reindex" : {"main_cmd"  :"dials.reindex"},
-                              "refine" : {"main_cmd"  :"dials.refine"},
-                              "integrate" : {"main_cmd"  :"dials.integrate"},
-                              "symmetry" : {"main_cmd"  :"dials.symmetry"},
-                              "scale" : {"main_cmd"  :"dials.scale"},
-                              "combine_experiments" : {"main_cmd"  :"dials.combine_experiments"}}
+        self.param_widgets = widgets_defs
 
         self.param_widgets["import"]["simple"] = imp_widg
         self.param_widgets["import"]["advanced"] = None
@@ -249,6 +281,8 @@ class MainObject(QObject):
         self.tree_obj = format_utils.TreeShow()
         self.tree_scene = TreeDirScene(self)
         self.window.treeView.setScene(self.tree_scene)
+
+        self.window.Next2RunLayout.addWidget(QLabel("teeeeeeest"))
 
         self.current_next_buttons = 0
         self.current_params_widget = 0
