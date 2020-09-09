5603e2c034b4e6f0b9f6d601fd37ff0a7970f0f7
luisodls <luis.fuentes-montero@diamond.ac.uk>
2020-09-09 16:02:53 +0100

    fixing control widget labe/title

--- src/client/main_client.py
+++ src/client/main_client.py
@@ -278,7 +278,7 @@ class MainObject(QObject):
         self.font_point_size = QFont().pointSize()
         big_f_size = int(self.font_point_size * 1.6)
         big_font = QFont("OldEnglish", pointSize = big_f_size, italic=True)
-        self.window.CurrentControlWidgetLabel.setFont(big_font)
+        #self.window.CurrentControlWidgetLabel.setFont(big_font)
 
         self.tree_scene.node_clicked.connect(self.on_node_click)
         self.window.RetryButton.clicked.connect(self.on_retry)
@@ -488,7 +488,7 @@ class MainObject(QObject):
         self.params2run = []
 
     def change_widget(self, str_key):
-        self.window.CurrentControlWidgetLabel.setText(str_key)
+        self.window.BoxControlWidget.setTitle(str_key)
         self.window.StackedParamsWidget.setCurrentWidget(
             self.param_widgets[str_key]["main_page"]
         )
