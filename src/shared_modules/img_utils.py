a3ff26ddd54cdbe57095cf77463397a878c6e802
luisodls <luis.fuentes-montero@diamond.ac.uk>
2021-03-01 19:50:47 +0000

    playing with img treatment ...

--- src/shared_modules/img_utils.py
+++ src/shared_modules/img_utils.py
@@ -17,8 +17,36 @@ def img_arr_gen(x_size, y_size):
     return arr_2d
 
 
+def generate_bunches(arr_in, n_times):
+    step_lst = []
+    for times in range(n_times):
+        local_step = times + 1
+        step_lst.append(2 ** local_step)
+
+    step_lst.reverse()
+
+    print(step_lst)
+
+    lst_ini_stp = []
+    ini = 0
+    for pos, loop_step in enumerate(step_lst):
+        print(loop_step)
+        ini_stp = (ini, loop_step)
+        lst_ini_stp.append(ini_stp)
+        ini = int(ini + loop_step / 2)
+        ini_stp = (ini, loop_step)
+        lst_ini_stp.append(ini_stp)
+
+    print(lst_ini_stp)
+
+
+
 if __name__ == "__main__":
-    img_arr = img_arr_gen(800, 400)
-    print("img_arr =", img_arr)
-    plt.imshow(np.transpose(img_arr) , interpolation = "nearest")
-    plt.show()
+
+    img_arr = img_arr_gen(18, 14)
+    print("img_arr =\n", img_arr)
+    #plt.imshow(img_arr, interpolation = "nearest")
+    #plt.show()
+    generate_bunches(img_arr, 3)
+
+
