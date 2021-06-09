0f449a6624e89775a5f32ec75ce0c1fac5d7b82c
luisolafg <luicub@gmail.com>
2021-06-09 12:54:01 +0100

    speeding generation of JSON file in c++

--- src/server/img_uploader/img_stream_ext.cpp
+++ src/server/img_uploader/img_stream_ext.cpp
@@ -11,32 +11,68 @@ using scitbx::af::flex_int;
 
 std::string img_arr_2_str(flex_double& data2d)
 {
-    std::string std_str = "";
+    /*
+     * Here we generate a very long string
+     * that contains and entire JSON file
+     * with the dimensions (d1, d2) and
+     * the array in "str_data"
+     */
     int d1 = data2d.accessor().all()[0];
     int d2 = data2d.accessor().all()[1];
-    int i, j;
+    int i, j, pos, pos_size;
+    double d_num;
+    char std_str[15];
+    int buff_size = d1 * d2 * 15 + 30;
+    std::cout << "buff_size =" << buff_size << "\n";
+    // creating a char buffer full of spaces
+    char * ch_buff;
+    ch_buff = new char[buff_size];
+    memset(ch_buff,' ',buff_size);
+    //pos will keep track of where to write next
+    pos = 0;
+    // first writing in memory string, starting with << { >>
+    strcpy(&ch_buff[pos], "{");
+    pos++;
+    // writing the << d1 >> field in JSON format, dimension #1
+    pos_size = sprintf( std_str, "\"d1\":%i,", d1);
+    strcpy(&ch_buff[pos], std_str);
+    pos = pos + pos_size;
+    // writing the << d2 >> field in JSON format, dimension #2
+    pos_size = sprintf( std_str, "\"d2\":%i,", d2);
+    strcpy(&ch_buff[pos], std_str);
+    pos = pos + pos_size;
+    // writing the left side of << str_data >> field
+    pos_size = sprintf( std_str, "\"str_data\":\"");
+    strcpy(&ch_buff[pos], std_str);
+    pos = pos + pos_size;
 
-    std::stringstream stream_data;
-    stream_data << std::fixed;
-    stream_data << std::setprecision(2);
-    stream_data.str("");
-    stream_data.clear();
-    stream_data << "{\"d1\": " << d1 << ",\"d2\": " << d2 << ", ";
-    stream_data << "\"str_data\": \"";
-
-    std::cout << "looping thru an array of: (" << d1 << ", " << d2 << ") ...\n";
+    // all pixel intensities should be written with a loop
+    std::cout << "looping thru (" << d1 << ", " << d2 << ") ... nums \n";
     for (i = 0; i < d1; i++) {
         for (j = 0; j < d2; j++) {
-            stream_data << data2d(i, j);
-            if(i < d1 - 1 or j < d2 - 1){
-                stream_data << ",";
-            };
+            // writing intensity
+            d_num = double(data2d(i, j));
+            pos_size = sprintf( std_str, "%.2f", d_num);
+            strcpy(&ch_buff[pos], std_str);
+            pos = pos + pos_size;
+            // writing coma
+            strcpy(&ch_buff[pos], ",");
+            pos++;
         }
     }
-    stream_data << "\"}";
-    std_str = stream_data.str();
-    std::cout << "... loop ended\n";
-    return std_str;
+    std::cout << "... Loop END\n";
+
+    // moving backwards to overwrite last coma
+    pos--;
+    // closing both: quotes and braces
+    pos_size = sprintf( std_str, "\"}");
+    strcpy(&ch_buff[pos], std_str);
+    pos = pos + pos_size;
+    // passing all char buffer to a std::string to returning it
+    std::string all_str((char *)ch_buff);
+    // clearing memory
+    delete ch_buff;
+    return all_str;
 }
 
 BOOST_PYTHON_MODULE(img_stream_ext)
