21188815dd823c28c1a80ed454d630c0be3684fe
luisodls <luis.fuentes-montero@diamond.ac.uk>
2021-09-04 11:33:10 +0100

    putting main server program inside a function call main

--- src/server/main_server.py
+++ src/server/main_server.py
@@ -1,5 +1,5 @@
 """
-DUI's command simple stacked widgets
+DUI clouds's server side main program
 
 Author: Luis Fuentes-Montero (Luiso)
 With strong help from DIALS and CCP4 teams
@@ -27,169 +27,168 @@ import json, os, zlib, sys
 
 import multi_node
 
+PORT = 45678
 HOST = "localhost"
 #HOST = "serverip"
 
-class ReqHandler(http.server.BaseHTTPRequestHandler):
-    def do_POST(self):
-        content_len = int(self.headers.get('Content-Length'))
-        post_body = self.rfile.read(content_len)
-        print("\n post_body =", post_body)
-        body_str = str(post_body.decode('utf-8'))
-        print("\n str_tst =", body_str)
-        url_dict = parse_qs(body_str)
-        print("\n url_dict =", url_dict)
-        try:
-            tmp_cmd2lst = url_dict["cmd_lst"]
-            print("tmp_cmd2lst =", tmp_cmd2lst)
 
-        except KeyError:
-            print("no command in request (KeyError)")
-            self.send_header('Content-type', 'text/plain')
-            self.end_headers()
-            self.wfile.write(bytes(
-                'no command in request (KeyError) \n', 'utf-8'
-            ))
-            self.wfile.write(bytes('/*EOF*/', 'utf-8'))
-            return
+def main():
+    class ReqHandler(http.server.BaseHTTPRequestHandler):
+        def do_POST(self):
+            content_len = int(self.headers.get('Content-Length'))
+            post_body = self.rfile.read(content_len)
+            print("\n post_body =", post_body)
+            body_str = str(post_body.decode('utf-8'))
+            print("\n str_tst =", body_str)
+            url_dict = parse_qs(body_str)
+            print("\n url_dict =", url_dict)
+            try:
+                tmp_cmd2lst = url_dict["cmd_lst"]
+                print("tmp_cmd2lst =", tmp_cmd2lst)
 
-        cmd_lst = []
-        for inner_str in tmp_cmd2lst:
-            cmd_lst.append(inner_str.split(" "))
-
-        nod_lst = []
-        try:
-            for inner_str in url_dict["nod_lst"]:
-                nod_lst.append(int(inner_str))
-
-        except KeyError:
-            print("no node number provided")
-
-        cmd_dict = {"nod_lst":nod_lst,
-                    "cmd_lst":cmd_lst}
-
-        try:
-            cmd_tree_runner.run_dict(cmd_dict, self)
-
-
-            print("sending /*EOF*/")
-            self.wfile.write(bytes('/*EOF*/', 'utf-8'))
-
-
-        except BrokenPipeError:
-            print("\n *** BrokenPipeError *** while sending EOF or JSON \n")
-
-        except ConnectionResetError:
-            print("\n *** ConnectionResetError *** while sending EOF or JSON \n")
-
-
-    def do_GET(self):
-
-        self.send_response(200)
-        url_path = self.path
-        print("\n url_path =", url_path)
-
-        print("\n urlparse(url_path) =", urlparse(url_path))
-
-        url_dict = parse_qs(urlparse(url_path).query)
-        print("\n url_dict =", url_dict)
-        try:
-            tmp_cmd2lst = url_dict["cmd_lst"]
-            print("tmp_cmd2lst =", tmp_cmd2lst)
-
-        except KeyError:
-            print("no command in request (KeyError)")
-            self.send_header('Content-type', 'text/plain')
-            self.end_headers()
-            self.wfile.write(bytes(
-                'no command in request (KeyError) \n', 'utf-8'
-            ))
-            self.wfile.write(bytes('/*EOF*/', 'utf-8'))
-            return
-
-        cmd_lst = []
-        for inner_str in tmp_cmd2lst:
-            cmd_lst.append(inner_str.split(" "))
-
-        nod_lst = []
-        try:
-            for inner_str in url_dict["nod_lst"]:
-                nod_lst.append(int(inner_str))
-
-        except KeyError:
-            print("no node number provided")
-
-        cmd_dict = {"nod_lst":nod_lst,
-                    "cmd_lst":cmd_lst}
-        print("parse_qs(urlparse(url_path).query", cmd_dict)
-
-        try:
-            #lst_out = []
-            lst_out = cmd_tree_runner.run_dict(cmd_dict, self)
-
-            if type(lst_out) is list or type(lst_out) is dict:
+            except KeyError:
+                print("no command in request (KeyError)")
                 self.send_header('Content-type', 'text/plain')
                 self.end_headers()
-                json_str = json.dumps(lst_out) + '\n'
-                self.wfile.write(bytes(json_str, 'utf-8'))
+                self.wfile.write(bytes(
+                    'no command in request (KeyError) \n', 'utf-8'
+                ))
+                self.wfile.write(bytes('/*EOF*/', 'utf-8'))
+                return
 
-            elif type(lst_out) is bytes:
-                byt_data = zlib.compress(lst_out)
-                siz_dat = str(len(byt_data))
-                print("size =", siz_dat)
+            cmd_lst = []
+            for inner_str in tmp_cmd2lst:
+                cmd_lst.append(inner_str.split(" "))
 
-                self.send_header('Content-type', 'application/zlib')
-                self.send_header('Content-Length', siz_dat)
-                self.end_headers()
+            nod_lst = []
+            try:
+                for inner_str in url_dict["nod_lst"]:
+                    nod_lst.append(int(inner_str))
 
-                self.wfile.write(bytes(byt_data))
+            except KeyError:
+                print("no node number provided")
 
-            else:
-                #type(lst_out) = <class 'str'> ,# html report
+            cmd_dict = {"nod_lst":nod_lst,
+                        "cmd_lst":cmd_lst}
+
+            try:
+                cmd_tree_runner.run_dict(cmd_dict, self)
+
+
+                print("sending /*EOF*/")
+                self.wfile.write(bytes('/*EOF*/', 'utf-8'))
+
+
+            except BrokenPipeError:
+                print("\n *** BrokenPipeError *** while sending EOF or JSON \n")
+
+            except ConnectionResetError:
+                print("\n *** ConnectionResetError *** while sending EOF or JSON \n")
+
+
+        def do_GET(self):
+
+            self.send_response(200)
+            url_path = self.path
+            print("\n url_path =", url_path)
+
+            print("\n urlparse(url_path) =", urlparse(url_path))
+
+            url_dict = parse_qs(urlparse(url_path).query)
+            print("\n url_dict =", url_dict)
+            try:
+                tmp_cmd2lst = url_dict["cmd_lst"]
+                print("tmp_cmd2lst =", tmp_cmd2lst)
+
+            except KeyError:
+                print("no command in request (KeyError)")
                 self.send_header('Content-type', 'text/plain')
                 self.end_headers()
-                try:
-                    f = open(lst_out, "r")
-                    str_lst = f.readlines()
-                    f.close()
-                    for lin in str_lst:
-                        self.wfile.write(bytes(lin, 'utf-8'))
+                self.wfile.write(bytes(
+                    'no command in request (KeyError) \n', 'utf-8'
+                ))
+                self.wfile.write(bytes('/*EOF*/', 'utf-8'))
+                return
 
-                    self.wfile.write(bytes("/*EOF*/", 'utf-8'))
+            cmd_lst = []
+            for inner_str in tmp_cmd2lst:
+                cmd_lst.append(inner_str.split(" "))
 
-                except FileNotFoundError:
-                    self.wfile.write(bytes("/*EOF*/", 'utf-8'))
+            nod_lst = []
+            try:
+                for inner_str in url_dict["nod_lst"]:
+                    nod_lst.append(int(inner_str))
+
+            except KeyError:
+                print("no node number provided")
+
+            cmd_dict = {"nod_lst":nod_lst,
+                        "cmd_lst":cmd_lst}
+            print("parse_qs(urlparse(url_path).query", cmd_dict)
+
+            try:
+                #lst_out = []
+                lst_out = cmd_tree_runner.run_dict(cmd_dict, self)
+
+                if type(lst_out) is list or type(lst_out) is dict:
+                    self.send_header('Content-type', 'text/plain')
+                    self.end_headers()
+                    json_str = json.dumps(lst_out) + '\n'
+                    self.wfile.write(bytes(json_str, 'utf-8'))
+
+                elif type(lst_out) is bytes:
+                    byt_data = zlib.compress(lst_out)
+                    siz_dat = str(len(byt_data))
+                    print("size =", siz_dat)
+
+                    self.send_header('Content-type', 'application/zlib')
+                    self.send_header('Content-Length', siz_dat)
+                    self.end_headers()
+
+                    self.wfile.write(bytes(byt_data))
+
+                else:
+                    #type(lst_out) = <class 'str'> ,# html report
+                    self.send_header('Content-type', 'text/plain')
+                    self.end_headers()
+                    try:
+                        f = open(lst_out, "r")
+                        str_lst = f.readlines()
+                        f.close()
+                        for lin in str_lst:
+                            self.wfile.write(bytes(lin, 'utf-8'))
+
+                        self.wfile.write(bytes("/*EOF*/", 'utf-8'))
+
+                    except FileNotFoundError:
+                        self.wfile.write(bytes("/*EOF*/", 'utf-8'))
 
 
-            print("sending /*EOF*/")
-            self.wfile.write(bytes('/*EOF*/', 'utf-8'))
+                print("sending /*EOF*/")
+                self.wfile.write(bytes('/*EOF*/', 'utf-8'))
 
-        except BrokenPipeError:
-            print("\n *** BrokenPipeError *** while sending EOF or JSON \n")
+            except BrokenPipeError:
+                print("\n *** BrokenPipeError *** while sending EOF or JSON \n")
 
-        except ConnectionResetError:
-            print("\n *** ConnectionResetError *** while sending EOF or JSON \n")
-
-
-
-def iter_dict(file_path):
-    file_name = file_path.split("/")[-1]
-    local_dict = {
-        "file_name": file_name, "file_path": file_path, "list_child": []
-    }
-    if os.path.isdir(file_path):
-        local_dict["isdir"] = True
-        for new_file_name in sorted(os.listdir(file_path)):
-            new_file_path = os.path.join(file_path, new_file_name)
-            local_dict["list_child"].append(iter_dict(new_file_path))
-
-    else:
-        local_dict["isdir"] = False
-
-    return local_dict
+            except ConnectionResetError:
+                print("\n *** ConnectionResetError *** while sending EOF or JSON \n")
 
 
-if __name__ == "__main__":
+    def iter_dict(file_path):
+        file_name = file_path.split("/")[-1]
+        local_dict = {
+            "file_name": file_name, "file_path": file_path, "list_child": []
+        }
+        if os.path.isdir(file_path):
+            local_dict["isdir"] = True
+            for new_file_name in sorted(os.listdir(file_path)):
+                new_file_path = os.path.join(file_path, new_file_name)
+                local_dict["list_child"].append(iter_dict(new_file_path))
+
+        else:
+            local_dict["isdir"] = False
+
+        return local_dict
 
     try:
         with open("run_data") as json_file:
@@ -220,7 +219,8 @@ if __name__ == "__main__":
     cmd_dict = multi_node.str2dic("display")
     cmd_tree_runner.run_dict(cmd_dict)
 
-    PORT = 45678
+
+
     with socketserver.ThreadingTCPServer((HOST, PORT), ReqHandler) as http_daemon:
         print("\n serving at: \n  { host:", HOST, " port:", PORT, "} \n")
         try:
@@ -229,3 +229,23 @@ if __name__ == "__main__":
         except KeyboardInterrupt:
             http_daemon.server_close()
 
+
+    def iter_dict(file_path):
+        file_name = file_path.split("/")[-1]
+        local_dict = {
+            "file_name": file_name, "file_path": file_path, "list_child": []
+        }
+        if os.path.isdir(file_path):
+            local_dict["isdir"] = True
+            for new_file_name in sorted(os.listdir(file_path)):
+                new_file_path = os.path.join(file_path, new_file_name)
+                local_dict["list_child"].append(iter_dict(new_file_path))
+
+        else:
+            local_dict["isdir"] = False
+
+        return local_dict
+
+
+if __name__ == "__main__":
+    main()
