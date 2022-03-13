dad0638317a7752d32c60997ba2c56961fe85c19
luisodls <luis.fuentes-montero@diamond.ac.uk>
2022-03-13 12:12:17 +0000

    working on install script ...

--- install_dials_n_dui2.sh
+++ install_dials_n_dui2.sh
@@ -1,6 +1,6 @@
 printf "=========================================\n"
 printf "#          INSTALLING DIALS v3.8.3      #\n"
-printf "=========================================\n"
+printf "=========================================\n\n"
 INI_DIR_PATH=$(pwd)
 mkdir dials_installer_tmp
 cd dials_installer_tmp
@@ -10,7 +10,7 @@ cd dials-installer
 ./install --prefix=$INI_DIR_PATH
 printf "=========================================\n"
 printf "#          INSTALLING DUI2 DEPS         #\n"
-printf "=========================================\n"
+printf "=========================================\n\n"
 cd $INI_DIR_PATH
 source dials-v3-8-3/dials_env.sh
 libtbx.conda install pyside2 -y
@@ -18,16 +18,19 @@ libtbx.conda install git -y
 ###################### libtbx.conda install -c conda-forge gxx does not seems to work
 printf "=========================================\n"
 printf "#          INSTALLING DUI2              #\n"
-printf "=========================================\n"
+printf "=========================================\n\n"
 git clone https://github.com/ccp4/DUI2.git
 cd DUI2/src/server/img_uploader/
-dials.python compyling_boost_ext.py
+dials.python compyling_boost_ext.py > compyling_ext.log
 printf "=========================================\n"
 printf "#          DIALS and DUI2 INSTALLED     #\n"
-printf "=========================================\n"
+printf "=========================================\n\n"
 cd $INI_DIR_PATH
 rm -rf dials_installer_tmp/
 printf "Setting up launchers ...\n"
+mkdir dui_cmd_tools
+cd dui_cmd_tools
+CMD_TOOLS_PATH=$(pwd)
 SET_DIALS_ENV="source $INI_DIR_PATH/dials-v3-8-3/dials_env.sh"
 CLIENT_EXE_CMD="dials.python $INI_DIR_PATH/DUI2/src/client/main_client.py"
 echo $SET_DIALS_ENV > dui_client
@@ -37,8 +40,11 @@ SERVER_EXE_CMD="dials.python $INI_DIR_PATH/DUI2/src/server/main_server.py"
 echo $SET_DIALS_ENV > dui_server
 echo $SERVER_EXE_CMD >> dui_server
 chmod +x dui_server
-printf " ... Done\n"
+printf " ... Done\n\n"
 printf "=========================================\n"
 printf "#          DIALS and DUI2 READY         #\n"
 printf "=========================================\n\n\n\n"
-printf "add: \"export PATH=$INI_DIR_PATH:\$PATH\" without quotes to your init bash shell\n\n"
+printf "To set enviromet to run DUI2(including Dials) type:\n\n"
+printf "  export PATH=$CMD_TOOLS_PATH:\$PATH  \n\n"
+printf "or add it to your init bash shell\n\n"
+cd $INI_DIR_PATH
