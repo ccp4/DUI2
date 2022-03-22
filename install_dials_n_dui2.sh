208ede3ea87d41bef59b06cecc5257457cad02ff
luisodls <luis.fuentes-montero@diamond.ac.uk>
2022-03-22 14:14:08 +0000

    adding to the installer one more command << dui all local >>

--- install_dials_n_dui2.sh
+++ install_dials_n_dui2.sh
@@ -35,16 +35,26 @@ rm -rf dials_installer_tmp/
 printf "Setting up launchers ...\n"
 mkdir dui_cmd_tools
 cd dui_cmd_tools
+
 CMD_TOOLS_PATH=$(pwd)
 SET_DIALS_ENV="source $INI_DIR_PATH/dials-v3-8-3/dials_env.sh"
+
 CLIENT_EXE_CMD="dials.python $INI_DIR_PATH/DUI2/src/client/main_client.py \$@"
 echo $SET_DIALS_ENV > dui_client
 echo $CLIENT_EXE_CMD >> dui_client
 chmod +x dui_client
+
 SERVER_EXE_CMD="dials.python $INI_DIR_PATH/DUI2/src/server/main_server.py \$@"
 echo $SET_DIALS_ENV > dui_server
 echo $SERVER_EXE_CMD >> dui_server
 chmod +x dui_server
+
+ALL_LOCAL_CMD_N1="dui_server all_local=true &"
+ALL_LOCAL_CMD_N2="dui_client all_local=true"
+echo $ALL_LOCAL_CMD_N1 > dui_all_local
+echo $ALL_LOCAL_CMD_N2 >> dui_all_local
+chmod +x dui_all_local
+
 printf " ... Done\n\n"
 printf "========================================\n"
 printf "#         DIALS and DUI2 READY         #\n"
@@ -52,5 +62,6 @@ printf "========================================\n\n"
 printf "To set enviromet to run DUI2 (including Dials) type:\n\n"
 printf "  export PATH=$CMD_TOOLS_PATH:\$PATH  \n\n"
 printf "or add it to your init bash shell\n\n"
+
 cd $INI_DIR_PATH
 cd ..
