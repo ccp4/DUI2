af730f9499927b6cbd66b803e00bb18b9f76cc9d
luisodls <luis.fuentes-montero@diamond.ac.uk>
2022-05-01 10:19:10 +0100

    removing temp comments

--- install_dials_n_dui2.sh
+++ install_dials_n_dui2.sh
@@ -26,72 +26,71 @@ libtbx.conda install -c conda-forge mamba -y
 
 printf "Setting up mamba ...\n"
 libtbx.refresh
-#printf "========================================\n"
-#printf "#         INSTALLING DUI2 DEPS         #\n"
-#printf "========================================\n\n"
-#libtbx.mamba install pyside2 -y
-#printf "========================================\n"
-#printf "#           INSTALLING DUI2            #\n"
-#printf "========================================\n\n"
-#
-#{ # try
-#    git clone https://github.com/ccp4/DUI2.git &&
-#    printf "  GIT use  \n\n "
-#} || { # catch
-#    wget https://github.com/ccp4/DUI2/archive/refs/heads/master.zip
-#    unzip master.zip
-#    mv DUI2-master DUI2
-#    rm master.zip
-#    printf "  using WGET intead of GIT  \n\n "
-#}
-#
-#cd DUI2/src/server/img_uploader/
-#printf "========================================\n"
-#printf "#     COMPYLING DUI2 C++ EXTENSION     #\n"
-#printf "========================================\n\n"
-#dials.python compyling_boost_ext.py
-#printf "========================================\n"
-#printf "#       DIALS and DUI2 INSTALLED       #\n"
-#printf "========================================\n\n"
-#cd $INI_DIR_PATH
-#rm -rf dials_installer_tmp/
-#printf "Setting up launchers ...\n"
-#mkdir dui_cmd_tools
-#cd dui_cmd_tools
-#
-#CMD_TOOLS_PATH=$(pwd)
-#
-#SET_DIALS_ENV="source $INI_DIR_PATH/dials-v3-9-0/dials_env.sh"
-#SET_W_ENG_FLAG="export QTWEBENGINE_CHROMIUM_FLAGS=\"--no-sandbox\""
-#
-#CLIENT_EXE_CMD="dials.python $INI_DIR_PATH/DUI2/src/client/main_client.py \$@"
-#echo $SET_W_ENG_FLAG > dui_client
-#echo $SET_DIALS_ENV >> dui_client
-#echo $CLIENT_EXE_CMD >> dui_client
-#chmod +x dui_client
-#
-#SERVER_EXE_CMD="dials.python $INI_DIR_PATH/DUI2/src/server/main_server.py \$@"
-#echo $SET_W_ENG_FLAG > dui_server
-#echo $SET_DIALS_ENV >> dui_server
-#echo $SERVER_EXE_CMD >> dui_server
-#chmod +x dui_server
-#
-#ALL_LOCAL_CMD_N1="dui_server all_local=true &"
-#ALL_LOCAL_CMD_N2="dui_client all_local=true"
-#echo $ALL_LOCAL_CMD_N1 > dui_all_local
-#echo $ALL_LOCAL_CMD_N2 >> dui_all_local
-#chmod +x dui_all_local
-#
-#printf " ... Done\n\n"
-#printf "========================================\n"
-#printf "#         DIALS and DUI2 READY         #\n"
-#printf "========================================\n\n"
-#printf " commands abailable: \n\n"
-#printf "   dui_all_local,  dui_server,  dui_client\n\n"
-#printf " To set enviromet to run DUI2 (including Dials) type:\n\n"
-#printf "   export PATH=$CMD_TOOLS_PATH:\$PATH  \n\n"
-#printf " or add it to your init bash shell\n\n"
-#
-#cd $INI_DIR_PATH
-#cd ..
-#
+printf "========================================\n"
+printf "#         INSTALLING DUI2 DEPS         #\n"
+printf "========================================\n\n"
+libtbx.mamba install pyside2 -y
+printf "========================================\n"
+printf "#           INSTALLING DUI2            #\n"
+printf "========================================\n\n"
+
+{ # try
+    git clone https://github.com/ccp4/DUI2.git &&
+    printf "  GIT use  \n\n "
+} || { # catch
+    wget https://github.com/ccp4/DUI2/archive/refs/heads/master.zip
+    unzip master.zip
+    mv DUI2-master DUI2
+    rm master.zip
+    printf "  using WGET intead of GIT  \n\n "
+}
+
+cd DUI2/src/server/img_uploader/
+printf "========================================\n"
+printf "#     COMPYLING DUI2 C++ EXTENSION     #\n"
+printf "========================================\n\n"
+dials.python compyling_boost_ext.py
+printf "========================================\n"
+printf "#       DIALS and DUI2 INSTALLED       #\n"
+printf "========================================\n\n"
+cd $INI_DIR_PATH
+rm -rf dials_installer_tmp/
+printf "Setting up launchers ...\n"
+mkdir dui_cmd_tools
+cd dui_cmd_tools
+
+CMD_TOOLS_PATH=$(pwd)
+
+SET_DIALS_ENV="source $INI_DIR_PATH/dials-v3-9-0/dials_env.sh"
+SET_W_ENG_FLAG="export QTWEBENGINE_CHROMIUM_FLAGS=\"--no-sandbox\""
+
+CLIENT_EXE_CMD="dials.python $INI_DIR_PATH/DUI2/src/client/main_client.py \$@"
+echo $SET_W_ENG_FLAG > dui_client
+echo $SET_DIALS_ENV >> dui_client
+echo $CLIENT_EXE_CMD >> dui_client
+chmod +x dui_client
+
+SERVER_EXE_CMD="dials.python $INI_DIR_PATH/DUI2/src/server/main_server.py \$@"
+echo $SET_W_ENG_FLAG > dui_server
+echo $SET_DIALS_ENV >> dui_server
+echo $SERVER_EXE_CMD >> dui_server
+chmod +x dui_server
+
+ALL_LOCAL_CMD_N1="dui_server all_local=true &"
+ALL_LOCAL_CMD_N2="dui_client all_local=true"
+echo $ALL_LOCAL_CMD_N1 > dui_all_local
+echo $ALL_LOCAL_CMD_N2 >> dui_all_local
+chmod +x dui_all_local
+
+printf " ... Done\n\n"
+printf "========================================\n"
+printf "#         DIALS and DUI2 READY         #\n"
+printf "========================================\n\n"
+printf " commands abailable: \n\n"
+printf "   dui_all_local,  dui_server,  dui_client\n\n"
+printf " To set enviromet to run DUI2 (including Dials) type:\n\n"
+printf "   export PATH=$CMD_TOOLS_PATH:\$PATH  \n\n"
+printf " or add it to your init bash shell\n\n"
+
+cd $INI_DIR_PATH
+cd ..
