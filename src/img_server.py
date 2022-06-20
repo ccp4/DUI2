89ca452db0fb3d75241332a3f4c269454d8e9ea1
luisodls <luis.fuentes-montero@diamond.ac.uk>
2022-06-20 10:56:06 +0100

    adding new module

---
+++ src/img_server.py
@@ -0,0 +1,29 @@
+from multiprocessing import Process, Pipe
+from server import image_browser_server
+
+server_par_def = (
+    ("init_path", None),
+    ("port", 45678),
+    ("host", "localhost"),
+    #("host", "serverip"),
+    ("all_local", "false"),
+)
+
+if __name__ == '__main__':
+    pipe_server_1, pipe_server_2 = Pipe()
+    prcs_serv = Process(
+        target = image_browser_server.main,
+        args = (server_par_def, pipe_server_2)
+    )
+    prcs_serv.start()
+    new_port = pipe_server_1.recv()
+    print("# time to launch client app with port =",  new_port, "\n")
+
+    par_def = (
+        ("url", 'http://localhost:' + str(new_port) + '/'),
+        ("all_local", "true"),
+    )
+
+    prcs_serv.join()
+    print("Closing server naturally")
+
