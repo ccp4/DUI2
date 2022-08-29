46c5d366f669418131daea1fb1a9742cbd869a01
luisodls <luis.fuentes-montero@diamond.ac.uk>
2022-08-29 17:14:38 +0100

    adding two more test installers

---
+++ dui_intaller_after_conda.sh
@@ -0,0 +1,24 @@
+echo " ################################################# "
+echo " #   simplified DUI2 installer for developers    # "
+echo " ################################################# "
+echo " "
+echo " "
+echo " installing git "
+echo " "
+conda install -c conda-forge git -y
+echo " "
+echo " installing dials "
+echo " "
+conda install -c conda-forge dials -y
+echo " "
+echo " installing DUI2 dependency PySide2  "
+echo " "
+conda install -c conda-forge pyside2 -y
+echo " "
+echo " cloning DUI2 code "
+echo " "
+git clone https://github.com/ccp4/DUI2.git
+L_DIR="$(pwd "$0")"
+echo " "
+echo "run: \"python $L_DIR/DUI2/src/all_local.py\" to launch DUI2 locally"
+echo " "
