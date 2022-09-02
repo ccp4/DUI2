cc4794aa90d449ff37b3be8bd74c679237bb58c4
luisodls <luis.fuentes-montero@diamond.ac.uk>
2022-09-02 19:45:28 +0100

    adding missing << windows_exe >> key, now in a client side

--- src/only_client.py
+++ src/only_client.py
@@ -5,6 +5,7 @@ if __name__ == '__main__':
     par_def = (
         ("url", 'http://localhost:45678/'),
         ("all_local", "false"),
+        ("windows_exe", "false"),
     )
 
     run_client.main(par_def)
