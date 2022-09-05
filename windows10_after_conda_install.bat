65589b03c900e579fa1b90374acd5477f090b8a7
luisodls <luis.fuentes-montero@diamond.ac.uk>
2022-09-05 17:00:05 +0100

    small fix on windows installer

--- windows10_after_conda_install.bat
+++ windows10_after_conda_install.bat
@@ -1,5 +1,12 @@
+ECHO ####################################################
 ECHO Installer of Dui2 starting from a Conda installation
+ECHO ####################################################
+ECHO Installing git
 conda install -c conda-forge git -y && ^
+ECHO Installing dials
 conda install -c conda-forge dials -y && ^
+ECHO Installing PySide2
 conda install -c conda-forge pyside2 -y && ^
-
+ECHO cloning Dui2 source code
+git clone https://github.com/ccp4/DUI2.git
+ECHO done
