37195f368e4da7266eff8bf63c9edfbb350b318c
rober-loxl <robert.loxley.i7@gmail.com>
2022-09-06 12:12:41 +0100

    small attempt to fix installer

--- windows10_after_conda_install.bat
+++ windows10_after_conda_install.bat
@@ -1,7 +1,7 @@
 conda install -c conda-forge git -y && ^
 conda install -c conda-forge dials -y && ^
 conda install -c conda-forge pyside2 -y && ^
-git clone https://github.com/ccp4/DUI2.git
+git clone https://github.com/ccp4/DUI2.git && ^
 set ini_dir=%cd%
 set full_sting=python %ini_dir%\src\all_local.py windows_exe=true
 echo %full_sting% > dui_all_local.bat
