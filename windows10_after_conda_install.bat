6daf9e6f333ad61f60b7e6ccc0e0241215fb5978
rober-loxl <robert.loxley.i7@gmail.com>
2022-09-06 11:35:04 +0100

    adding bat launcher to windows after conda installer

--- windows10_after_conda_install.bat
+++ windows10_after_conda_install.bat
@@ -2,3 +2,7 @@ conda install -c conda-forge git -y && ^
 conda install -c conda-forge dials -y && ^
 conda install -c conda-forge pyside2 -y && ^
 git clone https://github.com/ccp4/DUI2.git
+set ini_dir=%cd%
+set full_sting=python %ini_dir%\src\all_local.py windows_exe=true
+echo %full_sting% > dui_all_local.bat
+echo use the dui_all_local.bat script to launch Dui2
