629152dd053aa9bda6b7f8a4d53ba5c19d2644b4
luisodls <luis.fuentes-montero@diamond.ac.uk>
2022-10-12 15:27:29 +0100

    renaming installer

--- windows10_after_conda_install.bat
+++ windows10_after_conda_install.bat
@@ -3,6 +3,6 @@ call conda install -c conda-forge dials -y
 call conda install -c conda-forge pyside2 -y
 git clone https://github.com/ccp4/DUI2.git
 set ini_dir=%cd%
-set full_sting=python %ini_dir%\DUI2\src\all_local.py windows_exe=true
-echo %full_sting% > dui_all_local.bat
+set full_srting=python %ini_dir%\DUI2\src\all_local.py windows_exe=true
+echo %full_srting% > dui_all_local.bat
 echo use the dui_all_local.bat script to launch Dui2
