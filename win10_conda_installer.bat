903e311b20c866a4b271cf6a8c23b965d470359e
luisolafg <luicub@gmail.com>
2022-10-13 17:07:36 +0100

    removing obsolete installers

--- win10_conda_installer.bat
+++
@@ -1,4 +0,0 @@
-ECHO Downloading miniconda3
-curl -o miniconda_install.exe https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe
-ECHO running miniconda3 install
-miniconda_install.exe
--- windows10_after_conda_install.bat
+++
@@ -1,8 +0,0 @@
-call conda install -c conda-forge git -y
-call conda install -c conda-forge dials -y
-call conda install -c conda-forge pyside2 -y
-git clone https://github.com/ccp4/DUI2.git
-set ini_dir=%cd%
-set full_srting=python %ini_dir%\DUI2\src\all_local.py windows_exe=true
-echo %full_srting% > dui_all_local.bat
-echo use the dui_all_local.bat script to launch Dui2
