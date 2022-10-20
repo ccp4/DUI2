0864b2bec6c8354ac80479d460d7dfbbf57fbe79
luisodls <luis.fuentes-montero@diamond.ac.uk>
2022-10-20 23:08:07 +0100

    adding starting point to build multythreading gui

---
+++ src/shared_modules/all_local_gui_tst.py
@@ -0,0 +1,54 @@
+import sys, os, time
+from PySide2.QtCore import *
+from PySide2.QtWidgets import *
+from PySide2.QtGui import *
+from PySide2 import QtUiTools
+
+class my_one(QThread):
+    def __init__(self, times_in = 3):
+        super(my_one, self).__init__()
+        self.times = times_in
+
+    def run(self):
+        print("times entered =", self.times)
+        for rept in range(self.times):
+            print("rep n", rept, " of ", self.times)
+            time.sleep(1)
+
+
+class MultiRunner(QObject):
+    def __init__(self):
+        super(MultiRunner, self).__init__()
+        self.thread_lst = []
+
+    def run_one_work(self):
+        new_thread = my_one(len(self.thread_lst) + 1)
+        new_thread.start()
+        self.thread_lst.append(new_thread)
+
+
+class MainImgViewObject(QObject):
+    def __init__(self, parent = None):
+        super(MainImgViewObject, self).__init__(parent)
+        self.parent_app = parent
+        self.window = QtUiTools.QUiLoader().load("tmp_gui.ui")
+        self.window.setWindowTitle("test")
+        print("inside QObject")
+        self.m_run = MultiRunner()
+        self.window.RunPushButton.clicked.connect(self.run_one_clicked)
+        self.window.show()
+
+    def run_one_clicked(self):
+        print("run_one_clicked(MainImgViewObject)")
+        self.m_run.run_one_work()
+
+
+def main():
+    app = QApplication(sys.argv)
+    m_obj = MainImgViewObject(parent = app)
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
+
