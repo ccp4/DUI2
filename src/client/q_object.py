4b2f910803ac8ddea9b4a6499a6edac7b7ac452d
luisodls <luis.fuentes-montero@diamond.ac.uk>
2022-11-08 16:35:07 +0000

    test

--- src/client/q_object.py
+++ src/client/q_object.py
@@ -21,7 +21,7 @@ copyright (c) CCP4 - DLS
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-import os, sys, requests, time
+import os, sys, requests
 
 from PySide2.QtCore import *
 from PySide2.QtWidgets import *
