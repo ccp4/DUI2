f8b863922dbc893afb861000b142d7799718049a
luisodls <luis.fuentes-montero@diamond.ac.uk>
2023-06-17 19:04:59 +0100

    ready for tagging v0.999.4

--- src/shared_modules/_version.py
+++ src/shared_modules/_version.py
@@ -1,4 +1,4 @@
 # This file must be separately readable and depend on nothing else in DUI
 # Temp Format: 0.999.PATCH until releace
 # Future Format: YYYY.MM.PATCH
-__version__ = "0.999.3"
+__version__ = "0.999.4"
