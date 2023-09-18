45bf0ccbf53e9e34e9fb4db6c9bdfaeab3e3a540
luisodls <luis.fuentes-montero@diamond.ac.uk>
2023-09-18 19:45:28 +0100

    moving version format to YYYY.MM.PATH as we are ready to release

--- src/shared_modules/_version.py
+++ src/shared_modules/_version.py
@@ -1,4 +1,3 @@
 # This file must be separately readable and depend on nothing else in DUI
-# Temp Format: 0.999.PATCH until releace
-# Future Format: YYYY.MM.PATCH
-__version__ = "0.999.7"
+# Format: YYYY.MM.PATCH
+__version__ = "2023.09.1"
