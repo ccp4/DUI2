2f599091d7be1060a4abf02ec82ce23e4693080f
luisodls <luis.fuentes-montero@diamond.ac.uk>
2024-01-29 16:02:38 +0000

    fixing read me, leftover << v >>

--- dui_intaller_after_conda.sh
+++ dui_intaller_after_conda.sh
@@ -16,7 +16,7 @@ conda install -c conda-forge pyside2 -y
 echo " "
 echo " cloning DUI2 code "
 echo " "
-git clone -b v2024.01.29  https://github.com/ccp4/DUI2.git
+git clone -b 2024.01.29  https://github.com/ccp4/DUI2.git
 L_DIR="$(pwd "$0")"
 echo " "
 echo "run: \"python $L_DIR/DUI2/src/all_local.py\" to launch DUI2 locally"
