b45f0e2c60f374334a9182562ececf88d347a807
luisodls <luis.fuentes-montero@diamond.ac.uk>
2024-02-18 14:11:29 +0000

    adding python launcher for server app, tesing pyproject.toml

--- src/run_dui2_server.py
+++ src/run_dui2_server.py
@@ -4,5 +4,6 @@ import re
 import sys
 from dui2.only_server import main
 if __name__ == '__main__':
+    #consider removing the next line, as this is a C.L.I. app
     sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
     sys.exit(main())
