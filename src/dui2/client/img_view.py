dc05604fe661f4538aef68e97813d23cd8349f5a
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2024-05-31 16:04:36 +0100

    some cleaning after fixing the input dialog of the image viewer in isolated mode

--- src/dui2/client/img_view.py
+++ src/dui2/client/img_view.py
@@ -104,9 +104,6 @@ class LoadSliceMaskImage(QThread):
                   "path"    : self.exp_path,
                   "cmd_str" : my_cmd_lst}
 
-        print("\n my_cmd =", my_cmd, "\n")
-
-
         self.r_time_req = get_request_real_time(
             params_in = my_cmd, main_handler = self.my_handler
         )
@@ -738,14 +735,8 @@ class DoImageView(QObject):
 
     def __call__(self, in_img_num, nod_or_path = True):
         self.cur_img_num = in_img_num
-
-        #self.nod_or_path = "/tmp/run_dui2_nodes/run3/indexed.expt"
-
         self.nod_or_path = nod_or_path
         self.exp_path = None
-
-        print("self.nod_or_path =", self.nod_or_path)
-
         self.build_background_n_get_nod_num(self.cur_img_num)
 
     def set_just_imported(self):
@@ -1266,10 +1257,6 @@ class DoImageView(QObject):
 
             self.get_x1_y1_x2_y2()
             self.set_inv_scale()
-
-
-            print("self.cur_img_num =", self.cur_img_num)
-
             self.load_slice_image = LoadSliceImage(
                 unit_URL = self.uni_url,
                 nod_num_lst = [self.cur_nod_num],
@@ -1291,9 +1278,6 @@ class DoImageView(QObject):
             self.load_slice_image.start()
 
             # Now Same for mask
-
-
-
 
             self.load_slice_mask_image = LoadSliceMaskImage(
                 unit_URL = self.uni_url,
@@ -1630,9 +1614,6 @@ class MainImgViewObject(QObject):
         self.img_num_changed()
 
     def set_selection(self, str_select, isdir):
-
-        print("\n set_selection \n")
-
         self.nod_or_path = str_select
         self.dir_selected = isdir
         self.window.IntroPathEdit.setText(self.nod_or_path)
