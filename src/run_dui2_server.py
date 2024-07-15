174e016c5567258ead7ee8a1b2fa2b81c0ecdf2d
luisodls <luis.fuentes-montero@diamond.ac.uk>
2024-07-15 11:25:33 +0100

    testing adding some profiling from server side

--- src/run_dui2_server.py
+++ src/run_dui2_server.py
@@ -4,6 +4,12 @@ import re
 import sys
 from dui2.only_server import main
 if __name__ == '__main__':
+    tmp_off = '''
     #consider removing the next line, as this is a C.L.I. app
     sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
     sys.exit(main())
+    '''
+
+
+    import cProfile
+    cProfile.run('main()')
