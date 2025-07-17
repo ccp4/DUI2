c0110f35207c598e0f64912503c65ca1273d971a
luisodls <luis.fuentes-montero@@diamond.ac.uk>
2025-07-17 16:51:27 +0100

    changing version before tagging V= 2025.7.17

--- install_dui2_after_ccp4_w_curl.sh
+++ install_dui2_after_ccp4_w_curl.sh
@@ -4,7 +4,7 @@ INI_DIR_PATH=$(pwd)
 printf "========================================\n"
 printf "#           DOWNLOADING DUI2           #\n"
 printf "========================================\n\n"
-TAG_VER="2025.5.28"
+TAG_VER="2025.7.17"
 TO_CURL="https://github.com/ccp4/DUI2/archive/refs/tags/v$TAG_VER.zip"
 curl -L -O $TO_CURL
 printf "========================================\n"
