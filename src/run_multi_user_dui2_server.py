dbbf043e7918c2b83f95ca23dc454068f2816cef
luisodls <luis.fuentes-montero@@diamond.ac.uk>
2025-10-16 11:08:32 +0100

    adding launcher for multi user server

---
+++ src/run_multi_user_dui2_server.py
@@ -0,0 +1,10 @@
+# this is a copy / pasted script from a generated build by pip3
+
+import re
+import sys
+from dui2.multi_user.http_multi_user_system import main
+if __name__ == '__main__':
+    #consider removing the next line, as this is a C.L.I. app
+    sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
+    sys.exit(main())
+
