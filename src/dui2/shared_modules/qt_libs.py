3c601acad1caec646dc1f2b6bd22104bcc253cc6
luisodls <luis.fuentes-montero@diamond.ac.uk>
2026-03-05 12:56:50 +0000

    safer way to detect complete installation QtWebEngine

--- src/dui2/shared_modules/qt_libs.py
+++ src/dui2/shared_modules/qt_libs.py
@@ -21,39 +21,84 @@ copyright (c) CCP4 - DLS
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+import os
+import sys
+
+def is_webengine_functional(pyside_ver):
+    # 1. Check if module is even importable
+    try:
+        if pyside_ver == 6:
+            from PySide6 import QtWebEngineWidgets
+
+        else:
+            #assuming pyside_ver = 2
+            from PySide2 import QtWebEngineWidgets
+
+    except ImportError:
+        print("Here fail #1")
+        return False
+
+    # 2. Locate the helper executable
+    # Qt searches for this file to render web content
+    #process_path = QtCore.QLibraryInfo.location(QtCore.QLibraryInfo.LibraryExecutablesPath)
+    process_path = QLibraryInfo.location(QLibraryInfo.LibraryExecutablesPath)
+    print("process_path =", process_path)
+    if sys.platform == "win32":
+        filename = "QtWebEngineProcess.exe"
+
+    else:
+        filename = "QtWebEngineProcess"
+
+    #filename = "QtWebEngineProcess.exe" if sys.platform == "win32" else "QtWebEngineProcess"
+    full_path = os.path.join(process_path, filename)
+
+    if not os.path.exists(full_path):
+        # Path might be different in virtualenvs or bundled apps
+        # fallback check in standard QtWebEngineWidgets path
+        full_path = os.path.join(
+            os.path.dirname(QtWebEngineWidgets.__file__), "Qt", "bin", filename
+        )
+
+    if not os.path.exists(full_path):
+        print("Here fail #2")
+        return False
+
+    print("Here YES")
+    return True
+
+
+pyside_ver = 6
+
 try:
     from PySide6 import QtUiTools
     from PySide6.QtCore import *
     from PySide6.QtWidgets import *
     from PySide6.QtGui import *
     print("Using PySide6 as Qt bindings")
-    try:
-        from PySide6.QtWebEngineWidgets import QWebEngineView
-        from PySide6.QtWebEngineCore import QWebEngineSettings
+    if is_webengine_functional(pyside_ver):
+        try:
+            from PySide6.QtWebEngineWidgets import QWebEngineView
+            from PySide6.QtWebEngineCore import QWebEngineSettings
 
-    except (ImportError, ModuleNotFoundError):
-        print(
-            " Web Engine module not found, working with system web browser "
-        )
+        except (ImportError, ModuleNotFoundError):
+            print(
+                " Web Engine module not found, working with system web browser "
+            )
 
 except (ImportError, ModuleNotFoundError):
-
+    pyside_ver = 2
     from PySide2 import QtUiTools
     from PySide2.QtCore import *
     from PySide2.QtWidgets import *
     from PySide2.QtGui import *
     print("Using PySide2 as Qt bindings")
+    if is_webengine_functional(pyside_ver):
+        try:
+            from PySide2.QtWebEngineWidgets import (
+                QWebEngineView, QWebEngineSettings
+            )
 
-    try:
-        from PySide2.QtWebEngineWidgets import (
-            QWebEngineView, QWebEngineSettings
-        )
-
-    except (ImportError, ModuleNotFoundError):
-        print(
-            " Web Engine module not found, working with system web browser "
-        )
-
-should_be_imported_like_this = '''
-from dui2.shared_modules.qt_libs import *
-'''
+        except (ImportError, ModuleNotFoundError):
+            print(
+                " Web Engine module not found, working with system web browser "
+            )
