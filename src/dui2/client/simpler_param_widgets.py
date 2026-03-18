e50bb9598d4c939b94de345dc24a9290b3dbcd17
luisodls <luis.fuentes-montero@@diamond.ac.uk>
2026-03-18 17:43:14 +0000

    editing labels in export widget

--- src/dui2/client/simpler_param_widgets.py
+++ src/dui2/client/simpler_param_widgets.py
@@ -2203,12 +2203,15 @@ class ExportWidget(QWidget):
 
     def msg_on_transfer(self, msg_in):
         if msg_in == b'ok':
-            self.progress_label.setText("...")
+            self.progress_label.setStyleSheet("color: rgba(0, 0, 0, 255")
+            self.progress_label.setText(" CloudRun launched successfully ")
 
         else:
-            self.progress_label.setText(str(msg_in))
+            self.progress_label.setStyleSheet("color: rgba(255, 1, 1, 255)")
+            self.progress_label.setText(str(msg_in)[2:-1])
 
     def save_mtz_on_disc(self, mtz_info):
+        self.progress_label.setStyleSheet("color: rgba(0, 0, 0, 255")
         self.progress_label.setText("...")
         file_out = open(self.file_name, "wb")
         try:
